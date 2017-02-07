c40e308b65fd14c034444d7fd06a8e68947c8d95
dciangot <diego.ciangottini@cern.ch>
2017-02-07 16:42:35 +0000

    taskname regex + unused page removed

--- TaskStatus/forms.py
+++ TaskStatus/forms.py
@@ -3,7 +3,7 @@ from django.core import validators
 import re
 
 class NameForm(forms.Form):
-    reg = re.compile('^[a-z]+$')
+    reg = re.compile('^[0-9]{6}_[0-9]{6}:\S+_crab_\S+$')
     task_name = forms.CharField(label='Insert Taskname', 
                                 max_length=100, 
                                 validators=[validators.RegexValidator(regex=reg, message='Invalid taskname')])
